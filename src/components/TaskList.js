8501b4b7c9bbb821d5a43db3a2d35215782b305b
Doris Morales <moralesdoris880@gmail.com>
2021-12-20 16:32:03 -0500

    TaskList and Task Deliverable complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({TASKS, CATEGORIES}) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [tasks, setTasks] = useState(TASKS);
+
+  function handleDeleteTask(deleteTask){
+    const updatedTasks = tasks.filter((task)=> task.text !== deleteTask.text)
+    setTasks(updatedTasks);
+  }
+
+  const tasksToDisplay = tasks.filter((task)=>{
+    if(selectedCategory === "All") return true;
+
+    return task.category === selectedCategory;
+  });
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksToDisplay.map((task)=>(<Task task={task} onDeleteTask ={handleDeleteTask}/>))
+      }
     </div>
   );
 }
