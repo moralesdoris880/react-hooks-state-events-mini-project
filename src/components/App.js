5bddd9d4f7e647b84a9947d479a24bb1126dcea5
Doris Morales <moralesdoris880@gmail.com>
2021-12-20 16:43:14 -0500

    Added Category Buttons

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,21 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
+import { useState } from "react/cjs/react.development";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedCategory,setSelectedCategory] = useState("ALL")
+
+  function handleCategoryChange(category){
+    setSelectedCategory(category)
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter CATEGORIES={CATEGORIES} onCategoryChange={handleCategoryChange}/>
       <NewTaskForm />
       <TaskList TASKS={TASKS} CATEGORIES={CATEGORIES}/>
     </div>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({CATEGORIES, onCategoryChange}) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+      CATEGORIES.map((category)=>(
+        <button onClick={onCategoryChange}>{category}</button>
+      ))
+      }
     </div>
   );
 }
